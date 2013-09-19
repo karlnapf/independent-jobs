8a487420d5bc86e53f54a542991238c74dcd4188
Heiko Strathmann <heiko.strathmann@gmail.com>
2013-09-19 16:49:14 +0100

    moved parameter class to engines increase std memory to 2gb

--- engines/BatchClusterParameters.py
+++ engines/BatchClusterParameters.py
@@ -10,7 +10,7 @@ import logging
 
 class BatchClusterParameters(object):
     def __init__(self, foldername="", job_name_base="job_", \
-                 max_walltime=3600, nodes=1, memory=1, loglevel=logging.INFO):
+                 max_walltime=3600, nodes=1, memory=2, loglevel=logging.INFO):
         self.foldername = foldername
         self.job_name_base = job_name_base
         self.max_walltime = max_walltime
