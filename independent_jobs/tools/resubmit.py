aa78dc9d7f2b020527179c45b8dab151781cb16d
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-10-02 11:17:09 +0100

    fix readlines issue

--- independent_jobs/tools/resubmit.py
+++ independent_jobs/tools/resubmit.py
@@ -7,6 +7,6 @@ def resubmit(job_dir, batch_engine):
     
     fname = job_dir + "batch_script"
     with open(fname, "r") as f:
-        job_string = f.readlines()
+        job_string = "".join(f.readlines())
             
     batch_engine.submit_to_batch_system(job_string)
