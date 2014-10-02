a23609d593357b6a95e89041e78d80c29b92181f
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-10-02 10:24:17 +0100

    mini update

--- independent_jobs/tools/resubmit.py
+++ independent_jobs/tools/resubmit.py
@@ -1,4 +1,10 @@
+import os
+
+
 def resubmit(job_dir, batch_engine):
+    if not job_dir[-1] == os.sep:
+        job_dir += os.sep
+    
     fname = job_dir + "batch_script"
     with open(fname, "r") as f:
         job_string = f.readlines()
