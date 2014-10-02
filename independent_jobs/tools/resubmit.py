1e0d5f08dd9162723ffed190e21da3dc2ee5f193
Heiko Strathmann <heiko.strathmann@gmail.com>
2014-10-02 10:09:44 +0100

    change re-submission script to just take a directory

--- independent_jobs/tools/resubmit.py
+++ independent_jobs/tools/resubmit.py
@@ -1,26 +1,6 @@
-import glob
-import os
-import pickle
-
-from independent_jobs.tools.FileSystem import FileSystem
-from independent_jobs.tools.Log import logger
-
-def resubmit(job_dir, resubmit_pattern_match, batch_engine):
-    dirs = glob.glob(job_dir + "job*")
-        
-    resubmit = []
-    for d in dirs:
-        a_fname = d + os.sep + "aggregator.bin"
-        j_fname = d + os.sep + "job.bin"
-        if not FileSystem.file_exists_new_shell(a_fname):
-            with open(j_fname, "r") as f:
-                partial_posterior = pickle.load(f).partial_posterior
-                if resubmit_pattern_match(partial_posterior):
-                    logger.info("%s unfinished" % d.split(os.sep)[-1])
-                    resubmit.append(d)
-                    
-    for d in resubmit:
-        with open(d + os.sep + "batch_script", "r") as f:
-            job_string = f.readlines()
+def resubmit(job_dir, batch_engine):
+    fname = job_dir + "batch_script"
+    with open(fname, "r") as f:
+        job_string = f.readlines()
             
     batch_engine.submit_to_batch_system(job_string)
