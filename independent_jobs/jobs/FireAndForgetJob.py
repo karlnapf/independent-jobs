c6bb8e51eb3b9fe782deb1cad020021de7694a19
Heiko Strathmann <heiko.strathmann@gmail.com>
2017-03-24 19:59:33 +0000

    add extractor for best parameters

--- independent_jobs/jobs/FireAndForgetJob.py
+++ independent_jobs/jobs/FireAndForgetJob.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 import os
 import time
+import itertools
 
 from independent_jobs.aggregators.ScalarResultAggregator import ScalarResultAggregator
 from independent_jobs.aggregators.SingleResultAggregator import SingleResultAggregator
@@ -44,6 +45,67 @@ def store_results(fname=os.path.expanduser("~") + os.sep + "results.txt", **kwar
             print("IOError writing to csv ... trying again in %d." % sleep_time)
             time.sleep(1)
 
+def extract_array(fname, param_names, result_name="result",
+                  non_existing=np.nan, redux_funs=[np.mean], return_param_values=True):
+    """
+    Given a csv file (as e.g. product by FireAndForgetJob, extraxts an
+    array where each dimension corresponds to a provided parameter, and
+    each element is a redux (e.g. mean) of all results (of given same)
+    for the parameter combinations.
+    
+    A default value can be specified.
+    """
+    with open(fname) as f:
+        df = pd.read_csv(f)
+    
+    param_values = {}
+    for param in param_names:
+        values = np.sort(np.unique(df[param]))
+        values = values[~np.isnan(values)]
+        param_values[param] = values
+    
+    sizes = [len(param_values[param]) for param in param_names]
+    results = [np.zeros(tuple(sizes)) + non_existing for _ in redux_funs]
+    
+    all_combs = itertools.product(*[param_values[param] for param in param_names])
+    
+    for comb in all_combs:
+        masks = [df[param] == comb[i] for i, param in enumerate(param_names)]
+        
+        lines = df
+        for mask in masks:
+            lines = lines[mask]
+        
+        if len(lines) > 0:
+            for j, redux_fun in enumerate(redux_funs):
+                result = redux_fun(lines[result_name].values)
+                ind_array = tuple([np.where(param_values[param] == comb[i])[0][0] for i, param in enumerate(param_names)])
+                results[j][ind_array] = result
+
+    if not return_param_values:
+        return results
+    else:
+        return results, param_values
+
+def best_parameters(db_fname, param_names, result_name, selector=np.nanmin,
+                    redux_fun=np.nanmean, plot=False):
+    """
+    Extracts the best choice of parameters using @see extract_array
+    """
+    results, param_values = extract_array(db_fname,
+                            result_name=result_name,
+                            param_names=param_names,
+                            redux_funs=[redux_fun])
+    
+    results = results[0]
+
+    best_ind = np.unravel_index(np.nanargmin(results), results.shape)
+
+    best_params = {}
+    for i, param_name in enumerate(param_names):
+        best_params[param_name] = param_values[param_name][best_ind[i]]
+    
+    return best_params, selector(results)
 
 class FireAndForgetJob(IndependentJob):
     def __init__(self, db_fname, result_name="result", **param_dict):
